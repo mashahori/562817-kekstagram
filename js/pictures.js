52c7d9f89a7f5b4711da83d48dcb2370ad47e1b0
mashahori <mashahori@users.noreply.github.com>
2018-11-30 09:57:23 +0300

    Исправление Трэвиса 2

--- js/pictures.js
+++ js/pictures.js
@@ -25,7 +25,7 @@ var randomString = function (stringsArray, count) {
   var resultString = '';
   for (var i = 0; i < count; i++) {
     if (i != 0) {
-      resultString +== ' ';
+      resultString += ' ';
     }
     resultString += stringsArray[randomInteger(0, stringsArray.length - 1)];
   }
