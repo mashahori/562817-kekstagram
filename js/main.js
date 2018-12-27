ee7bb9e088e1991dbfa182eb3ff935f468a1a1c6
mashahori <mashahori@users.noreply.github.com>
2018-12-27 16:13:32 +0300

    Исправление ошибок Трэвиса

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@
   var ENTER_BUTTON = 13;
   var pictureClose = document.querySelector('.big-picture__cancel');
 
+  var onError = function () {
+    // console.error(message);
+  };
+
+  var onSuccess = function () {
+    // console.log(data);
+  };
+
   window.load('https://js.dump.academy/kekstagram/data', onSuccess, onError);
 
   window.similarListElement.addEventListener('click', window.showBigPicture);
