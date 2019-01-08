fa135ce4d318e53f30ad92800306149a89154521
mashahori <mashahori@users.noreply.github.com>
2019-01-08 14:07:07 +0300

    #27 Личный проект: шэр, лайк

---
+++ js/chosen-photo.js
@@ -0,0 +1,30 @@
+'use strict';
+
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+  var fileChooser = document.querySelector('.img-upload__input');
+  var previewWrapper = document.querySelector('.img-upload__preview');
+  var preview = previewWrapper.querySelector('img');
+
+  fileChooser.addEventListener('change', function () {
+    preview.src = '';
+    var file = fileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+     return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        preview.src = reader.result;
+        console.log(preview);
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+})();
